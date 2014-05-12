f6ad040de061e6626a951801674105a9c301dc70
gardnersmitha <austingardnersmith@gmail.com>
2014-05-11 21:43:52 -0400

    adding facebook like box

--- index.php
+++ index.php
@@ -1,17 +1,18 @@
 <?php require('header.php'); ?>
 
 		<div class="container-fluid homepage">
-				<!-- Hero Panel -->
+				<!-- Hero Row -->
 			<div class="row hero">
 				<img src="img/logo2014.png" class="img-responsive logo center-block" alt="2014 Logo">
 				<a href="enter.php" role="button" class="center-block cta btn btn-lg btn-success btn-block">Enter a Team</a>
 			</div>
 
-			<!-- History Panel -->
+			<!-- History Row -->
 			<div class="row history">
 				<div class="col-xs-8 col-sm-6 col-md-6 col-lg-2 col-xs-offset-2 col-sm-offset-3 col-md-offset-3 col-lg-offset-5 icon-wrap">
 					<img src="img/blueprint.png" class="img-responsive center-block" alt="Blueprint Graphic">
 				</div>
+
 				<div class="col-xs-12 col-sm-8 col-md-8 col-lg-8 col-sm-offset-2 col-md-offset-2 col-lg-offset-2">
 					<span class="label label-success center-block">ABOUT</span>
 					<h3 class="text-center panel-heading">It began innocently enough.</h3>
@@ -21,6 +22,37 @@
 					</p>
 				</div>
 			</div>
+
+			<!-- Facebook Row -->
+			<div class="row facebook">
+				<div class="fb-like-wrapper">
+					<div class="fb-like" data-href="https://www.facebook.com/CCinvitational" data-layout="box_count" data-action="like" data-show-faces="false" data-share="false" data-width="300"></div>
+				</div>
+				<h4 class="text-center">Go ahead, you click on that Like button.</h3>
+			</div>
+
+			<!-- Champions Row -->
+			<!-- <div class="row champions">
+				<div class="col-xs-8 col-sm-6 col-md-6 col-lg-2 col-xs-offset-2 col-sm-offset-3 col-md-offset-3 col-lg-offset-5 icon-wrap">
+					<img src="img/trophy.png" class="img-responsive center-block" alt="Trophy Graphic">
+				</div>
+
+				<div class="col-xs-12 col-sm-8 col-md-8 col-lg-8 col-sm-offset-2 col-md-offset-2 col-lg-offset-2">
+					<span class="label label-success center-block">PAST CHAMPIONS</span>
+					<h3 class="text-center panel-heading">To the victor go the spoils.</h3>
+					<p class="panel-body">Tournament champions take home a cash prize and a boatload of everylasting glory.
+					</p>
+				</div>
+
+				<div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 champs-wrap">
+					<img src="http://placekitten.com/1000/1000" class="img-responsive" alt="placeholder">
+				</div>
+
+				<div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 champs-wrap">
+					<img src="http://placekitten.com/1000/1000" class="img-responsive" alt="placeholder">
+				</div>
+			</div> -->
+
 		</div>
 
 <?php require('footer.php'); ?>
